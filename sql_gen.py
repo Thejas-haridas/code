ffb3b9e5a96c576bae069bdc8739d78e3f7aec9f
Aaryatyloones <aarya@tyloones.com>
2025-06-05 18:18:08 +0530

    Update sql_gen.py

--- sql_gen.py
+++ sql_gen.py
@@ -319,7 +319,7 @@ class Token(BaseModel):
     token_type: str
 
 class TokenData(BaseModel):
-    username: str | None = None
+    username: Optional[str] = None
 
 # Security
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
